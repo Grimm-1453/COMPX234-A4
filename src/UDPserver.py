abb0a364090418ee09fcc4403b8e9ca5904825ce
Grimm-1453 <xbc6016@outlook.com>
2025-06-11 18:53:12 +0800

    Update UDPserver.py
    [SERVER] Add multithreading support
    
    - Created separate thread for each DOWNLOAD request
    - Implemented handle_client skeleton (incomplete logic)

--- src/UDPserver.py
+++ src/UDPserver.py
@@ -1,5 +1,10 @@
 import socket
 import sys
+import threading
+
+def handle_client(file_path, client_addr, main_sock):
+    print(f"Handling request for {file_path} from {client_addr}")
+
 
 def main():
     port = int(sys.argv[1])
@@ -10,6 +15,12 @@ def main():
     while True:
         data, addr = sock.recvfrom(1024)
         print(f"Received: {data.decode()} from {addr}")
+        if msg[0] == "DOWNLOAD":
+          file_path = os.path.join("files/server", msg[1])
+          threading.Thread(
+            target=handle_client,
+            args=(file_path, addr, main_sock)
+          ).start()
 
 if __name__ == "__main__":
     main()
